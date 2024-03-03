d919756931e3b7d5a3683fe79182fd49dc5ef9c4
Anderson Silva <andinhosilva1@outlook.com.br>
2024-03-03 09:56:40 -0300

    Distância.

---
+++ URI1016/1016.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace URI
+{
+    class URI
+    {
+        static void Main()
+        {
+            double km = double.Parse(Console.ReadLine());
+
+            km = km * 2;
+
+            Console.WriteLine($"{km} minutos");
+        }
+    }
+}
