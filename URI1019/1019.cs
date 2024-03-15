c6be036159b0861bb06f491e6cbdd9a443b1b1af
Anderson Gomes da Silva <anderson.silva@fitbank.com.br>
2024-03-15 13:14:36 -0300

    Conversão de Tempo.

---
+++ URI1019/1019.cs
@@ -0,0 +1,16 @@
+using System;
+class URI {
+  static void Main() {
+
+    int N = int.Parse(Console.ReadLine());
+
+    int horas = N / 3600;
+
+    int minutos = (N % 3600 / 60);
+
+    int segundos = N % 60;
+      
+    Console.WriteLine($"{horas}:{minutos}:{segundos}");  
+    
+  }
+}
