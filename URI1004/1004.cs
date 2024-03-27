bd47df1c0c18223c92ed9e189cd4afabb2df3ac9
Anderson Silva <andinhosilva1@outlook.com.br>
2024-03-27 11:06:39 -0300

    Produto Simples.

---
+++ URI1004/1004.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+
+namespace URI
+{
+    class URI
+    {
+        static void Main()
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"PROD = {A * B}");
+        }
+    }
+}
