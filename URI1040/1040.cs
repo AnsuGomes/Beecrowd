b6437f0432f9cef2c88c650f0040ebafe146c146
Anderson Silva <andinhosilva1@outlook.com.br>
2024-03-27 22:48:16 -0300

    Média 3.

---
+++ URI1040/1040.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Globalization;
+
+namespace URI
+{
+    class URI
+    {
+        static void Main()
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            NumberFormatInfo numberFormat = culture.NumberFormat;
+            CultureInfo.CurrentCulture = culture;
+
+            string[] notas = Console.ReadLine().Split();
+
+            double nota1 = double.Parse(notas[0]);
+            double nota2 = double.Parse(notas[1]);
+            double nota3 = double.Parse(notas[2]);
+            double nota4 = double.Parse(notas[3]);
+
+            nota1 *= 2;
+            nota2 *= 3;
+            nota3 *= 4;
+            nota4 *= 1;
+
+            double media = (nota1 + nota2 + nota3 + nota4) / 10;
+
+            if(media == 4.85) //URI arredonda de 4.85 para 4.90
+                media -= 0.05;
+
+            if(media >= 7.0)
+                Console.WriteLine($"Media: {media:F1}\nAluno aprovado.");
+
+            else if (media < 5.0)
+                Console.WriteLine($"Media: {media:F1}\nAluno reprovado.");    
+
+            else {
+                double nota5 = double.Parse(Console.ReadLine());
+
+                double mediaRecuperacao = (media + nota5) / 2;
+
+                Console.WriteLine($"Media: {media:F1}\nAluno em exame.\nNota do exame: {nota5:F1}");
+
+                if (mediaRecuperacao >= 5.0)
+                    Console.WriteLine($"Aluno aprovado.\nMedia final: {mediaRecuperacao:F1}");
+                else
+                     Console.WriteLine($"Aluno reprovado.\nMedia final: {mediaRecuperacao:F1}");   
+            }    
+        }
+    }
+}
